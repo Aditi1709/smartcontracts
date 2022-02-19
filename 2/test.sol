21608695207a23a0131384846f39f0e54898dbbf
Aditi Chauhan <Aditi@APPLEs-MacBook-Pro.local>
2022-02-19 08:27:30 +0530

    unit test

---
+++ 2/test.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+import "remix_tests.sol";
+import "../contracts/1_Storage.sol";
+
+contract StorageTest {
+    Storage temp = new Storage();
+    function beforeAll() public {
+        // Here should instantiate tested contract
+        Assert.equal(uint(1), uint(1), "1 should be equal to 1");
+    }
+    function valueIsSet() public {
+        temp.store(1);
+        Assert.equal(temp.retrieve(),uint(1), "value is not 1");
+    }
+}
